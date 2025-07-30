b1532e3f85064433340f176dc73be9ec9660f08f
Oleg Ostanin <thistleman123@gmail.com>
2025-07-30 20:44:39 +0300

    Add routes

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
-package gotesttask
+package main
 
 import (
 	"archivezipper/handlers"
 	"archivezipper/task"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {	
@@ -11,4 +15,13 @@ func main() {
 	//NOTE Dependency injection
 	handlers.Init(taskManager)
 
+	r := mux.NewRouter()
+	r.HandleFunc("/tasks", handlers.CreateTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", handlers.GetTaskHandler).Methods("GET")
+	r.HandleFunc("tasks/{id}/files", handlers.AddFileHandler).Methods("POST")
+
+	log.Println("Server started on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
